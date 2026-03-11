bf99bfaa667f7be960262c0418e9ce6df3f4e967
imDMK <dominiks8318@gmail.com>
2026-03-11 13:54:44 +0100

    feat(playtime): emit PlayTimeChangedEvent when playtime is updated

--- playtime-core/src/main/java/com/github/imdmk/playtime/core/feature/playtime/PlayTimeService.java
+++ playtime-core/src/main/java/com/github/imdmk/playtime/core/feature/playtime/PlayTimeService.java
@@ -1,8 +1,10 @@
 package com.github.imdmk.playtime.core.feature.playtime;
 
 import com.github.imdmk.playtime.api.PlayTime;
+import com.github.imdmk.playtime.api.event.PlayTimeChangedEvent;
 import com.github.imdmk.playtime.core.injector.ComponentPriority;
 import com.github.imdmk.playtime.core.injector.annotations.Service;
+import com.github.imdmk.playtime.core.platform.event.EventCaller;
 import com.github.imdmk.playtime.core.platform.playtime.PlayTimeAdapter;
 import org.panda_lang.utilities.inject.annotations.Inject;
 
@@ -12,10 +14,12 @@ import java.util.UUID;
 public final class PlayTimeService {
 
     private final PlayTimeAdapter adapter;
+    private final EventCaller eventCaller;
 
     @Inject
-    PlayTimeService(PlayTimeAdapter adapter) {
+    public PlayTimeService(PlayTimeAdapter adapter, EventCaller eventCaller) {
         this.adapter = adapter;
+        this.eventCaller = eventCaller;
     }
 
     public PlayTime getCurrentPlayTime(PlayTimeUser user) {
@@ -23,8 +27,13 @@ public final class PlayTimeService {
         return playTime != null ? playTime : user.getPlayTime();
     }
 
-    public void setPlayTime(PlayTimeUser user, PlayTime playTime) {
-        user.setPlayTime(playTime);
-        adapter.write(user.getUuid(), playTime);
+    public void setPlayTime(PlayTimeUser user, PlayTime newPlayTime) {
+        UUID uuid = user.getUuid();
+        PlayTime oldPlayTime = user.getPlayTime();
+
+        user.setPlayTime(newPlayTime);
+        adapter.write(uuid, newPlayTime);
+
+        eventCaller.callEvent(new PlayTimeChangedEvent(uuid, newPlayTime, oldPlayTime));
     }
 }
